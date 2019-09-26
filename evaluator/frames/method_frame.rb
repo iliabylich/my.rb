35956f536728ed50af5dee50b21ca515506014ef
Ilya Bylich <ibylich@gmail.com>
2019-09-26 19:35:18 +0300

    extracted method arguments into separate class

--- evaluator/frames/method_frame.rb
+++ evaluator/frames/method_frame.rb
@@ -1,3 +1,5 @@
+require_relative "../helpers/method_arguments"
+
 MethodFrame = FrameClass.new do
   attr_reader :kwoptarg_ids
 
@@ -10,100 +12,14 @@ MethodFrame = FrameClass.new do
 
     # init arguments (without optargs and restarg)
 
-    args_info = _iseq[11].dup
+    args_info = _iseq[11]
 
-    id = 3
+    @kwoptarg_ids, @labels_to_skip = MethodArguments.new(
+      iseq: _iseq,
+      values: arg_values,
+      locals: locals
+    ).extract
 
-    # this array contains all arguments mixed with utility vars to extract kwargs/mlhs
-    arg_names.reverse_each do |arg_name|
-      arg_name += 1 if arg_name.is_a?(Integer)
-      locals.declare(name: arg_name, id: id)
-      id += 1
-    end
-
-    req_args_count = args_info[:lead_num]
-    opt_info = args_info[:opt].dup
-    rest_start = args_info[:rest_start]
-    kwdata = args_info[:keyword]
-    needs_kw = kwdata && kwdata.any?
-    kwvalues = nil
-    needs_rest = !rest_start.nil?
-
-    if needs_kw && kwvalues.nil?
-      kwvalues = arg_values.pop
-
-      unless kwvalues.is_a?(Hash)
-        raise "expected kwargs"
-      end
-    end
-
-    arg_names.each_with_index do |arg_name, idx|
-      if req_args_count && req_args_count > 0
-        # req argument
-        arg_name += 1 if arg_name.is_a?(Integer)
-        arg_value = arg_values.shift
-        locals.find(name: arg_name).set(arg_value)
-        req_args_count -= 1
-      elsif opt_info.any? && rest_start && idx < rest_start
-        if arg_values.any?
-          arg_value = arg_values.shift
-          locals.find(name: arg_name).set(arg_value)
-
-          # skip default value initialization
-          labels_to_skip << opt_info.first
-        end
-
-        opt_info.shift
-      elsif needs_rest
-        rest_value = arg_values
-        locals.find(name: arg_name).set(rest_value)
-        needs_rest = false
-      else
-        # kwreq/kwopt/kwres
-        kwarg_info = kwdata.detect { |name| name == arg_name }
-        kwoptarg_info = kwdata.detect { |(name,_)| name == arg_name }
-
-        if kwarg_info
-          # kwarg
-          if kwvalues.key?(arg_name)
-            arg_value = kwvalues.delete(arg_name)
-            locals.find(name: arg_name).set(arg_value)
-          else
-            raise "missing kwarg #{arg_name.inspect}"
-          end
-        elsif kwoptarg_info
-          # kwoptarg
-          local = locals.find(name: arg_name)
-
-          if kwvalues.key?(arg_name)
-            # value given
-            arg_value = kwvalues.delete(arg_name)
-            local.set(arg_value)
-
-            # skip default value initialization
-            # labels_to_skip << opt_info.first
-          elsif kwoptarg_info.length == 2
-            # no value in arglist, but simple inlined default value
-            _, default = *kwoptarg_info
-            local.set(default)
-
-            # skip default value initialization
-            labels_to_skip << opt_info.first
-          else
-            # no value in arglist, complex default value is set via insn in the future
-          end
-
-          opt_info.shift
-        else
-          value = kwvalues
-          locals.find(id: arg_name).set(value)
-
-          # All other variables are getting post-processed via insns
-          break
-        end
-      end
-    end
-
-    @kwoptarg_ids = (args_info[:keyword] || []).grep(Array).map { |name,| locals.find(name: name).id }
+    args = nil
   end
 end
---
+++ evaluator/helpers/method_arguments.rb
@@ -0,0 +1,112 @@
+class MethodArguments
+  attr_reader :arg_names, :values, :locals, :args_info
+
+  def initialize(iseq:, values:, locals:)
+    @values = values.dup
+    @locals = locals
+
+    @arg_names = iseq[10].dup
+    @args_info = iseq[11].dup
+  end
+
+  def extract
+    labels_to_skip = []
+
+    # this array contains all arguments mixed with utility vars to extract kwargs/mlhs
+    arg_names.reverse_each.with_index(3) do |arg_name, idx|
+      arg_name += 1 if arg_name.is_a?(Integer) # virtual attribute that holds mlhs value
+      locals.declare(name: arg_name, id: idx)
+    end
+
+    req_args_count = args_info[:lead_num] || 0
+
+    opt_info = args_info[:opt].dup
+
+    rest_start = args_info[:rest_start]
+    needs_rest = !rest_start.nil?
+
+    post_num = args_info[:post_num] || 0
+    post_start = args_info[:post_start]
+
+    kwdata = args_info[:keyword]
+    needs_kw = kwdata && kwdata.any?
+    kwvalues = nil
+
+    if needs_kw && kwvalues.nil?
+      kwvalues = values.pop
+
+      unless kwvalues.is_a?(Hash)
+        raise "expected kwargs"
+      end
+    end
+
+    arg_names.each_with_index do |arg_name, idx|
+      if req_args_count > 0
+        # req pos argument
+        arg_name += 1 if arg_name.is_a?(Integer)
+        arg_value = values.shift
+        locals.find(name: arg_name).set(arg_value)
+        req_args_count -= 1
+      elsif opt_info.any? && (rest_start ? idx < rest_start : true) && (post_start ? idx < post_start : true)
+        if values.any?
+          arg_value = values.shift
+          locals.find(name: arg_name).set(arg_value)
+
+          # skip default value initialization
+          labels_to_skip << opt_info.first
+        end
+
+        opt_info.shift
+      elsif needs_rest
+        rest_value = values
+        locals.find(name: arg_name).set(rest_value)
+        needs_rest = false
+      elsif post_num > 0
+        arg_name += 1 if arg_name.is_a?(Integer)
+        arg_value = values.shift
+        locals.find(name: arg_name).set(arg_value)
+        post_num -= 1
+      elsif kwdata && (kwarg_info = kwdata.detect { |name| name == arg_name })
+        # kwarg
+        if kwvalues.key?(arg_name)
+          arg_value = kwvalues.delete(arg_name)
+          locals.find(name: arg_name).set(arg_value)
+        else
+          raise "missing kwarg #{arg_name.inspect}"
+        end
+      elsif kwdata && (kwoptarg_info = kwdata.detect { |(name,_)| name == arg_name })
+        # kwoptarg
+        local = locals.find(name: arg_name)
+
+        if kwvalues.key?(arg_name)
+          # value given
+          arg_value = kwvalues.delete(arg_name)
+          local.set(arg_value)
+        elsif kwoptarg_info.length == 2
+          # no value in arglist, but simple inlined default value in insn
+          _, default = *kwoptarg_info
+          local.set(default)
+
+          # skip default value initialization
+          labels_to_skip << opt_info.first
+        else
+          # no value in arglist, complex default value is set via insn in the future
+        end
+
+        opt_info.shift
+      elsif kwdata
+        # kwrestarg
+        locals.find(id: arg_name).set(kwvalues)
+        kwdata = nil
+      else
+
+        # All other variables are getting post-processed via insns, that's the end
+        break
+      end
+    end
+
+    kwoptarg_ids = (@args_info[:keyword] || []).grep(Array).map { |name,| locals.find(name: name).id }
+
+    [kwoptarg_ids, labels_to_skip]
+  end
+end
